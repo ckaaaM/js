327bbb3bf0fb2d3ef51755e003f1c04f117bce46
ckaaaM <klikerxd12@gmail.com>
2024-09-13 11:02:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let hour = 24
+let minute = 60
+let sec = 60
+let day = 30
+let year = 365
+
+console.log("количество секунд в сутках:", hour*minute*sec)
+console.log("секунд в 30 сутках:", hour*30*minute*sec)
+console.log("количество секнуд в году:", hour*365*minute*sec)
+console.log("количество минут в сутках:", hour*minute)
+console.log("количество минту в году:", minute*hour*year )
+
+let bytes = 1
+let kilobytes = 1024
+let megabytes = 1024
+let gigabytes = 1024
+
+console.log("байт в мегабайте:", bytes*kilobytes*megabytes )
+console.log("количество байт в гигабайте:", bytes*kilobytes*megabytes*gigabytes)
+console.log("количество байт в 10 гигабайтах:" )
+
+

9857c329d015f6a32619c68a6217251b8e908572
ckaaaM <klikerxd12@gmail.com>
2024-09-16 23:29:38 +0500

    JS dz

--- app.js
+++ app.js
@@ -10,13 +10,16 @@ console.log("количество секнуд в году:", hour*365*minute*se
 console.log("количество минут в сутках:", hour*minute)
 console.log("количество минту в году:", minute*hour*year )
 
-let bytes = 1
+let bit = 1 // так понял бит тоже надо было прописать, чтобы был полный список системы едениц  ¯ \ _ (ツ) _ / ¯
+let bytes = 8
 let kilobytes = 1024
 let megabytes = 1024
 let gigabytes = 1024
+let terabytes = 1024
 
 console.log("байт в мегабайте:", bytes*kilobytes*megabytes )
 console.log("количество байт в гигабайте:", bytes*kilobytes*megabytes*gigabytes)
-console.log("количество байт в 10 гигабайтах:" )
-
+console.log("количество байт в 10 гигабайтах:", bytes*kilobytes*megabytes*gigabytes*10)
+console.log("количество байт в терабайте:", bytes*kilobytes*megabytes*gigabytes*terabytes)
+console.log("количество килобайт в терабайте:", kilobytes*megabytes*gigabytes*terabytes)
 

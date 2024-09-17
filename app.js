c2ef7b52e1cb35c50d447b9095573330f5890008
ckaaaM <klikerxd12@gmail.com>
2024-09-17 09:14:52 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-let r = 6
-const pi = 3.14
-let s = pi * r **2
-console.log (`площадь круга при радиусе ${r} равна ${s}`)
+let hour = 24
+let minute = 60
+let sec = 60
+let day = 30
+let year = 365
 
-let a = 6
-s = a ** 2
-console.log (`плщадь квадрата со стороной ${a} равна ${s}`)
 
-let b = 9
-s = a * b
-console.log(`площадь прямоугольника со стороной прямоугольника ${a} и ${b} равна ${s}`)
+console.log("количество секунд в сутках:", hour*minute*sec)
+console.log("секунд в 30 сутках:", hour*30*minute*sec)
+console.log("количество секнуд в году:", hour*365*minute*sec)
+console.log("количество минут в сутках:", hour*minute)
+console.log("количество минту в году:", minute*hour*year )
 
-// делать 67 задание № 4, 5, 6
+let bit = 1 // так понял бит тоже надо было прописать, чтобы был полный список системы едениц  ¯ \ _ (ツ) _ / ¯
+let bytes = 8
+let kilobytes = 1024
+let megabytes = 1024
+let gigabytes = 1024
+let terabytes = 1024
 
+console.log("байт в мегабайте:", bytes*kilobytes*megabytes )
+console.log("количество байт в гигабайте:", bytes*kilobytes*megabytes*gigabytes)
+console.log("количество байт в 10 гигабайтах:", bytes*kilobytes*megabytes*gigabytes*10)
+console.log("количество байт в терабайте:", bytes*kilobytes*megabytes*gigabytes*terabytes)
+console.log("количество килобайт в терабайте:", kilobytes*megabytes*gigabytes*terabytes)
 
 
